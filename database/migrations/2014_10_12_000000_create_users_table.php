b0036d246dfc13a49fd960bd2f837e2bcdb040b8
datata <dani@danitarazona.dev>
2023-12-13 12:37:09 +0100

    update: add role enum to users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password', '50');
+            $table->enum('role', ["user", "admin", "super_admin"])->default('user');
             $table->timestamps();
+            // $table->timestamp('created_at')->useCurrent();
+            // $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
