570cb6d6bcee3e4b504597be89292d3563210dc9
datata <dani@danitarazona.dev>
2023-12-14 11:48:05 +0100

    update: response controllers

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,27 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Providers\FoundationServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\Response;
 
 class TaskController extends Controller
 {
     public function createTask(Request $request)
     {
-        return 'CREATE TASKS';
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Task created successfully',
+                'data' => 'data'
+            ],
+            Response::HTTP_CREATED
+        );
     }
 
     public function getAllTasks(Request $request)
     {
-        return 'GET ALL TASKS';
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Retrieved Tasks successfully',
+                'data' => 'data'
+            ],
+            Response::HTTP_OK
+        );
     }
 
     public function updateTaskById(Request $request, $id)
     {
-        return 'UPDATA TASKS: ' . $id;
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Task updated successfully',
+                'data' => 'data'
+            ],
+            Response::HTTP_OK
+        );
     }
 
     public function deleteTaskById(Request $request, $id)
     {
-        return 'UPDATA TASKS: ' . $id;
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Task deketed successfully',
+                'data' => 'data'
+            ],
+            Response::HTTP_OK
+        );
     }
 }
