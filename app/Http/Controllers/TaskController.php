567c401671477ae0221afd4a86920218a1c969b4
datata <dani@danitarazona.dev>
2023-12-14 12:17:36 +0100

    update: create task with query builder

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Providers\FoundationServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -11,14 +12,42 @@ class TaskController extends Controller
 {
     public function createTask(Request $request)
     {
+      try {
+          // validar la informacion
+
+        // recoger y tratar esa informacion si es necesario
+        $title = $request->input('title');
+        $description = $request->input('description');
+        $userId = $request->input('user_id');
+
+        // Guardamos en BD
+        $newTask = DB::table('tasks')->insert(
+            [
+                'title' => $title,
+                'description' => $description,
+                'user_id' => $userId
+            ]
+        );
+
+        // devolver una respuesta
         return response()->json(
             [
                 'success' => true,
                 'message' => 'Task created successfully',
-                'data' => 'data'
+                'data' => $newTask
             ],
             Response::HTTP_CREATED
         );
+      } catch (\Throwable $th) {
+        return response()->json(
+            [
+                'success' => false,
+                'message' => 'Cant create Task',
+                'error' => $th->getMessage()
+            ],
+            Response::HTTP_INTERNAL_SERVER_ERROR
+        );
+      }
     }
 
     public function getAllTasks(Request $request)
